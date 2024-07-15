0cfb9e9bcea3815dc6b4c6a2a023f93c9539892a
viniciuspiotto <viniciuspiotto.dev@gmail.com>
2024-07-15 18:30:43 -0300

    feat(board): add security layer with exceptions for piece insertion
    Implement a security layer that includes exceptions to handle errors when inserting
    pieces into the board. This ensures the integrity of the board by validating piece
    placement and preventing invalid operations.

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,6 +6,9 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int cols) {
+        if (rows < 1 || cols < 1) {
+            throw new BoardException("Error creating board: rows and cols must be greater than 0");
+        }
         this.rows = rows;
         this.cols = cols;
         this.pieces = new Piece[rows][cols];
@@ -15,28 +18,44 @@ public class Board {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getCols() {
         return cols;
     }
 
-    public void setCols(int cols) {
-        this.cols = cols;
-    }
-
     public Piece piece(int row, int col) {
+        if (!positionExists(row, col)) {
+            throw new BoardException("Error creating piece: position does not exist");
+        }
         return pieces[row][col];
     }
 
     public Piece piece(Position pos) {
+        if (!positionExists(pos)) {
+            throw new BoardException("Error creating piece: position does not exist");
+        }
         return pieces[pos.getRow()][pos.getCol()];
     }
 
     public void placePiece(Piece piece, Position pos) {
+        if (thereIsPiece(pos)) {
+            throw new BoardException("Error creating piece: there is already a piece in position " + pos);
+        }
         pieces[pos.getRow()][pos.getCol()] = piece;
         piece.position = pos;
+    }
+
+    private boolean positionExists(int row, int col) {
+        return row >= 0 && row < rows && col >= 0 && col < cols;
+    }
+
+    public boolean positionExists(Position pos) {
+        return positionExists(pos.getRow(), pos.getCol());
+    }
+
+    public boolean thereIsPiece(Position pos) {
+        if (!positionExists(pos)) {
+            throw new BoardException("Position does not exist");
+        }
+        return piece(pos) != null;
     }
 }
