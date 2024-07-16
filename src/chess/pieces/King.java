09c2f8b35e1dac13c77c3f164a9a6f8267b2c6d0
viniciuspiotto <viniciuspiotto.dev@gmail.com>
2024-07-16 19:37:51 -0300

    feat(King): implement movement validation for king positions
    Add functionality to check and validate all possible positions a king can move
    to, based on its unique movement rules in chess, including one-square moves in
    any castling. This ensures the king's movements adhere to the game's legal
    restrictions and strategic elements.

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -15,8 +16,61 @@ public class King extends ChessPiece {
         return "K";
     }
 
+    private boolean canMove(Position pos) {
+        ChessPiece p = (ChessPiece)getBoard().piece(pos);
+        if (p == null) {
+            return true;
+        }
+        return p.getColor() != getColor();
+    }
+
     @Override
     public boolean[][] possibleMoves() {
-        return new boolean[getBoard().getRows()][getBoard().getCols()];
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getCols()];
+
+        Position p = new Position(0,0);
+
+        // above
+        p.setValues(position.getRow() - 1, position.getCol());
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getCol()] = true;
+        }
+        // below
+        p.setValues(position.getRow() + 1, position.getCol());
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getCol()] = true;
+        }
+        // left
+        p.setValues(position.getRow(), position.getCol() - 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getCol()] = true;
+        }
+        // right
+        p.setValues(position.getRow(), position.getCol() + 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getCol()] = true;
+        }
+        // nw
+        p.setValues(position.getRow() - 1, position.getCol() - 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getCol()] = true;
+        }
+        // ne
+        p.setValues(position.getRow() - 1, position.getCol() + 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getCol()] = true;
+        }
+        // sw
+        p.setValues(position.getRow() + 1, position.getCol() - 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getCol()] = true;
+        }
+        // se
+        p.setValues(position.getRow() + 1, position.getCol() + 1);
+        if (getBoard().positionExists(p) && canMove(p)) {
+            mat[p.getRow()][p.getCol()] = true;
+        }
+
+        return mat;
     }
 }
