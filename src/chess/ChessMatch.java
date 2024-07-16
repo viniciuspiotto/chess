e9c861139deb58506e97ed4323357bddcfe70b68
viniciuspiotto <viniciuspiotto.dev@gmail.com>
2024-07-16 18:37:29 -0300

    feat(chess): add validation for piece destination based on movement rules
    Introduce validation logic to ensure that the destination of a chess piece complies
    with its specific movement rules. This ensures that each move adheres to the legal
    parameters defined for different types of pieces, enchancing the game's rule enforcement.

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -32,6 +32,7 @@ public class ChessMatch {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
         return (ChessPiece) capturedPiece;
     }
@@ -42,6 +43,12 @@ public class ChessMatch {
         }
         if (!board.piece(pos).isThereAnyPossibleMove()) {
             throw new ChessException("There is no possible move on source position");
+        }
+    }
+
+    private void validateTargetPosition(Position source, Position target) {
+        if (!board.piece(source).possibleMove(target)) {
+            throw new ChessException("There is no possible move on target position");
         }
     }
 
