2dc94e70e6e951de65d026e964096c8a3faee1c8
viniciuspiotto <viniciuspiotto.dev@gmail.com>
2024-07-18 20:28:44 -0300

    feat(bishop): implement movement validation for bishop positions
    The bishop has a movement logic similar to ther rook, with the difference that it moves diagonally
    instead of along straight lines.

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -2,6 +2,7 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Pawn;
 import chess.pieces.Rook;
@@ -190,7 +191,9 @@ public class ChessMatch {
 
     private void initialSetup() {
         placeNewPiece('h', 1, new Rook(board, Color.RED));
+        placeNewPiece('c', 1, new Bishop(board, Color.RED));
         placeNewPiece('e', 1, new King(board, Color.RED));
+        placeNewPiece('f', 1, new Bishop(board, Color.RED));
         placeNewPiece('a', 1, new Rook(board, Color.RED));
         placeNewPiece('a', 2, new Pawn(board, Color.RED));
         placeNewPiece('b', 2, new Pawn(board, Color.RED));
@@ -202,8 +205,10 @@ public class ChessMatch {
         placeNewPiece('h', 2, new Pawn(board, Color.RED));
 
         placeNewPiece('e', 8, new King(board, Color.BLUE));
-        placeNewPiece('h', 8, new Rook(board, Color.BLUE));
         placeNewPiece('a', 8, new Rook(board, Color.BLUE));
+        placeNewPiece('c', 8, new Bishop(board, Color.BLUE));
+        placeNewPiece('h', 8, new Rook(board, Color.BLUE));
+        placeNewPiece('f', 8, new Bishop(board, Color.BLUE));
         placeNewPiece('a', 7, new Pawn(board, Color.BLUE));
         placeNewPiece('b', 7, new Pawn(board, Color.BLUE));
         placeNewPiece('c', 7, new Pawn(board, Color.BLUE));
